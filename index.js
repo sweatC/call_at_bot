8587321ccfc867066cda0eecd9d168f53b27d95b
Ilya Novohatskyi <1llyanovohatskyy@gmail.com>
2019-03-02 11:46:09 +0200

    Add calendar UI support

--- index.js
+++ index.js
@@ -2,4 +2,26 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 
-console.log(process.env)
+const Telegraf = require('telegraf');
+const session = require('telegraf/session');
+const Calendar = require('telegraf-calendar-telegram');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const calendar = new Calendar(bot);
+
+bot.use(session())
+
+calendar.setDateListener((context, date) => context.session.date = date);
+
+bot.command('new', context => {
+  const today = new Date();
+	const minDate = new Date();
+	minDate.setMonth(today.getMonth() - 2);
+	const maxDate = new Date();
+	maxDate.setMonth(today.getMonth() + 2);
+	maxDate.setDate(today.getDate());
+
+	context.reply('New Event', calendar.setMinDate(minDate).setMaxDate(maxDate).getCalendar())
+});
+
+bot.startPolling();
